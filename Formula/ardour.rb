b0a56dba8b6935ed258131ad36755fdd44734175
Danner Claflin <danner@addgene.com>
2024-05-29 21:05:11 +0000

    update ardour tap to 8.6

--- Formula/ardour.rb
+++ Formula/ardour.rb
@@ -1,46 +1,54 @@
 class Ardour < Formula
   desc "A digital audio workstation"
   homepage "https://ardour.org/"
-  url "https://github.com/Ardour/ardour/archive/refs/tags/8.6.tar.gz"
-  version "8.6"
-  sha256 "84d79d73d749cf05a223feb74d5fdfcacfef520e185169752e33fb9bfcbce7b3" # Replace with actual SHA256 hash of the 8.6 tarball
-  head "https://github.com/Ardour/ardour.git", branch: "8.6"
+  url "https://github.com/Ardour/ardour.git",
+      tag: "8.6",
+      revision: "0aefb67766ff3b8ee8c68dd3d75bda96048b43b9"
 
-  depends_on "pkg-config" => :build
+
   depends_on "aubio"
   depends_on "boost"
   depends_on "fftw"
   depends_on "glib"
   depends_on "glibmm"
-  depends_on "gtkmm"
   depends_on "jack"
   depends_on "libarchive"
   depends_on "liblo"
   depends_on "libsndfile"
   depends_on "libusb"
   depends_on "lrdf"
-  depends_on "lilv"
   depends_on "lv2"
   depends_on "pangomm"
+  depends_on "pkg-config" => :build
   depends_on "rubberband"
   depends_on "serd"
   depends_on "sord"
   depends_on "sratom"
+  depends_on "gtkmm"
+  depends_on "lilv"
   depends_on "taglib"
   depends_on "vamp-plugin-sdk"
-
-  def install
-    # Check if .waf is available before executing waf commands
-    waf_path = "./waf"
-    if !File.executable?(waf_path)
-      odie ".waf is not available. Please make sure it is installed and accessible."
-    end
     
-    system waf_path, "configure", "--prefix=#{prefix}", "--with-backends=jack"
-    system waf_path, "install"
+  def install
+    # Clone Ardour repository
+    system "git", "clone", "https://github.com/Ardour/ardour.git", "source"
+    # Switch to the specific tag
+    Dir.chdir("source") do
+      system "git", "checkout", "8.6"
+    end
 
-    cd "./tools/osx_packaging" do
-      system waf_path, "--help"
+    # Build Ardour
+    cd "source" do
+      system "./waf", "configure"
+      system "./waf"
+
+      # Install Ardour
+      system "./waf", "install"
+
+      # Create .app bundle
+      cd "tools/osx_packaging" do
+        system "./osx_build", "--public"
+      end
     end
   end
 
